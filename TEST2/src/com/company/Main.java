7ba8d0347cf86ee7c2046e8b0eb498aa237cc83e
BUCSZ <shenqinghao92@gmail.com>
2017-06-30 22:53:51 -0700

    1

---
+++ TEST2/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        double a = 92.92;
+        System.out.println(a);
+    }
+}
